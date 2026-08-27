7bf87b82828d02c80e4202aef20498c50772d3fd
jmcglone <jmcglone@umich.edu>
2026-08-27 11:57:33 -0400

    add CMS previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,31 @@
 import CMS from "decap-cms-app";
 import LinkFileComponent from "./components/linkFileComponent";
 import LinkFileButtonComponent from "./components/linkFileButtonComponent";
+import "./preview.scss";
+import BasicPagePreview from "./previews/basicPagePreview";
+import CardPreview from "./previews/cardPreview";
+import OurTeamPreview from "./previews/ourTeamPreview";
 
 CMS.registerEditorComponent(LinkFileComponent);
 CMS.registerEditorComponent(LinkFileButtonComponent);
+
+CMS.registerPreviewTemplate("cards", CardPreview);
+CMS.registerPreviewTemplate("our-team", OurTeamPreview);
+
+[
+  "about",
+  "our-principles",
+  "proposal-books",
+  "proposal-book-series",
+  "proposal-journals",
+  "contact",
+  "peer-review",
+  "research-integrity",
+  "authorship",
+  "help",
+  "faqs",
+  "terms",
+  "privacy",
+].forEach((name) => {
+  CMS.registerPreviewTemplate(name, BasicPagePreview);
+});
