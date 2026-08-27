90021108931f5b03d0e49f34901982995e7071fe
jmcglone <jmcglone@umich.edu>
2026-08-27 10:57:35 -0400

    update cms config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,28 +4,3 @@ import LinkFileButtonComponent from "./components/linkFileButtonComponent";
 
 CMS.registerEditorComponent(LinkFileComponent);
 CMS.registerEditorComponent(LinkFileButtonComponent);
-
-window.CMS_MANUAL_INIT = true;
-
-if (process.env.BRANCH === "preview") {
-  console.log("---- USING PREVIEW BACKEND ----")
-  CMS.init({
-    config: {
-      backend: {
-        name: 'github',
-        repo: 'mlibrary/about-aberdeenunipress',
-        branch: 'preview'
-      }
-    }
-  });
-} else {
-  CMS.init({
-    config: {
-      backend: {
-        name: 'github',
-        repo: 'mlibrary/about-aberdeenunipress',
-        branch: 'main'
-      }
-    }
-  });
-}
